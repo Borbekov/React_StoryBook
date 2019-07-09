dc7760b77c031b5b7487905ea536903b35dffc41
Borbekov <borbekov.b@gmail.com>
2019-07-09 23:44:09 +0600

    render navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLink from "./SignedInLink";
 import SignedOutLink from "./SignedOutLink";
+import { connect } from "react-redux";
 
-const Navbar = () => {
+const Navbar = props => {
+  const links = props.auth.uid ? <SignedInLink /> : <SignedOutLink />;
   return (
     <nav>
       <div className="nav-wrapper red lighten-1">
@@ -11,12 +13,17 @@ const Navbar = () => {
           <Link to="/" className="brand-logo">
             StoriesBook
           </Link>
-          <SignedInLink />
-          <SignedOutLink />
+          {links}
         </div>
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth
+  };
+};
+
+export default connect(mapStateToProps)(Navbar);
