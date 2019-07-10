57e2a4457e3eaa109a20bfbe2115e7540cfa24d8
Borbekov <borbekov.b@gmail.com>
2019-07-10 21:56:23 +0600

    sign up user

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -31,3 +31,34 @@ export const signOut = () => {
       });
   };
 };
+
+export const signUp = newUser => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+      .then(response => {
+        return firestore
+          .collection("users")
+          .doc(response.user.uid)
+          .set({
+            firstName: newUser.firstName,
+            secondName: newUser.secondName,
+            initials: newUser.firstName[0] + newUser.secondName[0]
+          });
+      })
+      .then(() => {
+        dispatch({
+          type: "SIGNUP_SUCCESS"
+        });
+      })
+      .catch(error => {
+        dispatch({
+          type: "SIGNUP_ERROR",
+          payload: error
+        });
+      });
+  };
+};
